da665564459e614f23fc7c7f1c911ac933e384ca
Jacob Jin <Jin@sil.org>
2016-06-29 14:19:41 -0400

    remove compiled js file
    Please enter the commit message for your changes. Lines starting

--- app/appProducer/mock-bloomBook.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.BLOOMBOOKS = [
-    { id: 11, title: "A Dancer's Tale", copyright: "Pratham", note: "Modified", language: ["english"] },
-    { id: 12, title: "Curious baby elephant", copyright: "SIL", note: "", language: ["english", "spanish"] },
-    { id: 13, title: "Disability is not inability", copyright: "ASBP", note: "", language: ["english", "spanish"] },
-    { id: 14, title: "Paper Play", copyright: "Pratham", note: "Unavailable", language: ["english"] },
-    { id: 15, title: "Best Book NA", copyright: "JacobJin", note: "You have to get it", language: ["chinese", "english"] }
-];
-//# sourceMappingURL=mock-bloomBook.js.map
