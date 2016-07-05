33969434c1f9308daa4ab18621ccd7c2d8fe5c9a
Jacob Jin <Jin@sil.org>
2016-07-05 10:26:08 -0400

    add mock service for user and app info

--- app/appProducer/mock-userInfo.ts
+++ app/appProducer/mock-userInfo.ts
@@ -1,5 +1,6 @@
 import { UserInfo } from './userInfo'
 
 export var USERINFOS: UserInfo[] = [
-    {id: 999, username: "Jacob", apps:[1]}
+    {id: 999, username: "Jacob", apps:[1,2]},
+    {id: 1, username: "Dr.Troll", apps:[]}
 ];
