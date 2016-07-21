0c32b756d9a6b6b843e8b7482f1ccd8c75b8db87
Jacob Jin <jacobjin1995@gmail.com>
2016-07-21 08:54:54 -0400

    add header for API authorization

---
+++ src/app/appProducer/appProducer.headers.ts
@@ -0,0 +1,6 @@
+import {Headers} from '@angular/http';
+
+export const AppProducerHeaders = new Headers();
+AppProducerHeaders.append('X-Parse-Application-Id','yrXftBF6mbAuVu3fO6LnhCJiHxZPIdE7gl1DUVGR');
+AppProducerHeaders.append('X-Parse-REST-API-Key', 'KZA7c0gAuwTD6kZHyO5iZm0t48RplaU7o3SHLKnj');
+
