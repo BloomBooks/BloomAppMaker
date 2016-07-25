77c0823ec6014e060e6ba7de1e3f98c1d607bbf7
Jacob Jin <jacobjin1995@gmail.com>
2016-07-25 13:34:32 -0400

    on route changes, also changes title

--- src/app/navBar/nav.component.ts
+++ src/app/navBar/nav.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'nav-bar',
     templateUrl: './nav.tpl.html'
 })
 
-export class NavBar {}
+export class NavBar {
+    title: string;
+
+    constructor(public router: Router) {
+        this.router.events.subscribe((val) => {
+            if (val.url === "/") {
+                this.title = "App Maker";
+            } else if (val.url === "/about") {
+                this.title = "About Bloom App Maker";
+            }
+        });
+    }
+}
